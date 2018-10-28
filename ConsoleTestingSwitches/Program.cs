a9973125d13926c583ddaeb51e5d86b499b15e26
Shiarisu <shiarisu@gmail.com>
2018-10-28 20:33:51 +0100

    Add project files.

---
+++ ConsoleTestingSwitches/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestingSwitches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("whatevs");
+            String tmp = Console.ReadLine();
+            tmp.Split(' ');
+            foreach (var item in tmp.Split(' '))
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.WriteLine(tmp);
+
+        }
+    }
+}
