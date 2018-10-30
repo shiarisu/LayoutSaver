c1a6c54a6f8db542dc4944a14cdfc840dce37ab6
Shiarisu <shiarisu@gmail.com>
2018-10-30 16:34:47 +0100

    basic program structure

--- ConsoleTestingSwitches/Program.cs
+++ ConsoleTestingSwitches/Program.cs
@@ -8,17 +8,52 @@ namespace ConsoleTestingSwitches
 {
     class Program
     {
+        public static string pos_x { get; private set; }
+        public static string pos_y { get; private set; }
+        public static string height { get; private set; }
+        public static string width { get; private set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("whatevs");
-            String tmp = Console.ReadLine();
-            tmp.Split(' ');
-            foreach (var item in tmp.Split(' '))
-            {
-                Console.WriteLine(item);
+            var p = new Program();
 
+            int i = 0;
+            while (i < args.Length)
+            {
+                Console.WriteLine("arg: " + args[i]);
+                var command = args[i];
+                switch (command)
+                {
+                    case "-x":
+                        Console.WriteLine(command + " " + args[++i]);
+                        pos_x = args[i];
+                        break;
+                    case "-y":
+                        Console.WriteLine(command + " " + args[++i]);
+                        pos_y = args[i];
+                        break;
+                    case "-h":
+                        Console.WriteLine(command + " " + args[++i]);
+                        height = args[i];
+                        break;
+                    case "-w":
+                        Console.WriteLine(command + " " + args[++i]);
+                        width = args[i];
+                        break;
+                    default:
+                        if (i == args.Length - 1)
+                        {
+                            Console.WriteLine("process name: " + command);
+
+                        }
+                        else
+                            Console.WriteLine("unknown parameters");
+                        break;
+                }
+
+
+                ++i;
             }
-            Console.WriteLine(tmp);
 
         }
     }
