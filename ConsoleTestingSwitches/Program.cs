3ce2711d3c414d9511438f82c3caf07c9fae9b13
Shiarisu <shiarisu@gmail.com>
2018-10-30 20:50:38 +0100

    make -f switch work, cleanup property names

--- ConsoleTestingSwitches/Program.cs
+++ ConsoleTestingSwitches/Program.cs
@@ -3,76 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using windows_layout_save;
 
 namespace ConsoleTestingSwitches
 {
     class Program
     {
-        public int pos_x { get; private set; } = 0;
-        public int pos_y { get; private set; } = 0;
-        public int height { get; private set; } = 600;
-        public int width { get; private set; } = 600;
-        public string procname { get; private set; }
-        public bool forced { get; private set; } = false;
+        public int PosX { get; private set; } = 0;
+        public int PosY { get; private set; } = 0;
+        public int Height { get; private set; } = 600;
+        public int Width { get; private set; } = 600;
+        public string Procname { get; private set; }
+        public bool Forced { get; private set; } = false;
 
         static void Main(string[] args)
         {
             var p = new Program();
 
-            int i = 0, tmp = 0;
+            int i = 0, tmp = 0, coordsSet = 0;
             while (i < args.Length)
             {
-                Console.WriteLine("arg: " + args[i]);
+                //Console.WriteLine("arg: " + args[i]);
                 var command = args[i];
                 switch (command)
                 {
                     case "-f":
-                        p.forced = true;
+                        p.Forced = true;
                         break;
                     case "-x":
-                        Console.WriteLine(command + " " + args[++i]);
+                        coordsSet |= 1;
+                        ++i;
+                        //Console.WriteLine(command + " " + args[i]);
                         if (!int.TryParse(args[i], out tmp))
                         {
                             Console.Error.WriteLine("wrong parameter, input integer");
                             return;
                         }
-                        p.pos_x = tmp;
+                        p.PosX = tmp;
                         break;
                     case "-y":
-                        Console.WriteLine(command + " " + args[++i]);
+                        coordsSet |= 2;
+                        ++i;
+                        //Console.WriteLine(command + " " + args[i]);
                         if (!int.TryParse(args[i], out tmp))
                         {
                             Console.Error.WriteLine("wrong parameter, input integer");
                             return;
                         }
-                        p.pos_y = tmp;
+                        p.PosY = tmp;
                         break;
                     case "-h":
-                        Console.WriteLine(command + " " + args[++i]);
+                        coordsSet |= 4;
+                        ++i;
+                        //Console.WriteLine(command + " " + args[i]);
                         if (!int.TryParse(args[i], out tmp))
                         {
                             Console.Error.WriteLine("wrong parameter, input integer");
                             return;
                         }
-                        p.height = tmp;
+                        p.Height = tmp;
                         break;
                     case "-w":
-                        Console.WriteLine(command + " " + args[++i]);
+                        coordsSet |= 8;
+                        ++i;
+                        //Console.WriteLine(command + " " + args[i]);
                         if (!int.TryParse(args[i], out tmp))
                         {
                             Console.Error.WriteLine("wrong parameter, input integer");
                             return;
                         }
-                        p.width = tmp;
+                        p.Width = tmp;
                         break;
                     default:
                         if (i == args.Length - 1)
                         {
-                            Console.WriteLine("process name: " + command);
-                            p.procname = command;
+                            //Console.WriteLine("process name: " + command);
+                            p.Procname = command;
                         }
                         else
-                            Console.Error.WriteLine("unknown parameters");
+                            Console.Error.WriteLine("unknown parameter: " + command);
                         break;
                 }
 
@@ -81,13 +90,22 @@ namespace ConsoleTestingSwitches
             }
 
             p.WriteSettings();
+            Console.WriteLine();
+            //if forced mode is off and not all of xyhw coords are set by the user 
+            if (!p.Forced && coordsSet != 15)
+            {
+                Console.Error.WriteLine("Provide all xyhw coordinates or use -f flag to force using default values");
+                return;
+            }
+            WindowsResizer wr = new WindowsResizer();
+            wr.resize(p.PosX, p.PosY, p.Width, p.Height, p.Procname);
 
+            Console.WriteLine();
         }
 
         private void WriteSettings()
         {
-            Console.WriteLine("x:" + this.pos_x + " y:" + this.pos_y + " h:" + this.height + " w:" + this.width + " proc:" + this.procname + " forced: " + this.forced);
-
+            Console.WriteLine("x:" + this.PosX + " y:" + this.PosY + " h:" + this.Height + " w:" + this.Width + " process name: \"" + this.Procname +"\"");
         }
     }
 }
---
+++ ConsoleTestingSwitches/WindowsResizer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+static class NativeMethods
+{
+    [DllImport("user32.dll", SetLastError = true)]
+
+    static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    public static extern bool SetWindowText(IntPtr hwnd, String lpString);
+
+
+    // Define the FindWindow API function.
+    [DllImport("user32.dll", EntryPoint = "FindWindow",
+        SetLastError = true)]
+    public static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly,
+        string lpWindowName);
+
+    // Define the SetWindowPos API function.
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool SetWindowPos(IntPtr hWnd,
+        IntPtr hWndInsertAfter, int X, int Y, int cx, int cy,
+        SetWindowPosFlags uFlags);
+
+    // Define the SetWindowPosFlags enumeration.
+    [Flags()]
+    public enum SetWindowPosFlags : uint
+    {
+        SynchronousWindowPosition = 0x4000,
+        DeferErase = 0x2000,
+        DrawFrame = 0x0020,
+        FrameChanged = 0x0020,
+        HideWindow = 0x0080,
+        DoNotActivate = 0x0010,
+        DoNotCopyBits = 0x0100,
+        IgnoreMove = 0x0002,
+        DoNotChangeOwnerZOrder = 0x0200,
+        DoNotRedraw = 0x0008,
+        DoNotReposition = 0x0200,
+        DoNotSendChangingEvent = 0x0400,
+        IgnoreResize = 0x0001,
+        IgnoreZOrder = 0x0004,
+        ShowWindow = 0x0040,
+    }
+}
+
+namespace windows_layout_save
+{
+    class WindowsResizer
+    {
+
+        public void resize(int x, int y, int width, int height, string procname)
+        {
+            // Get the target window's handle.
+            IntPtr target_hwnd =
+                NativeMethods.FindWindowByCaption(IntPtr.Zero, procname);
+            if (target_hwnd == IntPtr.Zero)
+            {
+                Console.Error.WriteLine(
+                    "Could not find a window with the title \"" +
+                    procname + "\"");
+                return;
+            }
+
+            NativeMethods.SetWindowPos(target_hwnd, IntPtr.Zero,
+                x, y, width, height, 0);
+        }
+    }
+}
